3e88bd53a92a7d5ca544573bc0859f73a474de8f
Andrew Stakhov <andrew.stakhov@gmail.com>
2019-03-28 18:36:09 -0400

    Add project files.

---
+++ dotnet-ers-refarch/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace dotnet_ers_refarch.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
